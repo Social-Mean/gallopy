29a7e96e1c4ee68be575a5f283ff23825ce1022f
Social Mean <2052760@tongji.edu.cn>
2023-07-06 22:10:34 +0800

    add: 共有的画图预设

---
+++ src/gallopy/rcParams.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+plt.rcParams["xtick.direction"] = "in"
+plt.rcParams["ytick.direction"] = "in"
+plt.rcParams["font.sans-serif"] = "Times New Roman"
+# plt.rcParams["font.size"] = 6
+plt.rcParams["text.usetex"] = False
+plt.rcParams["mathtext.fontset"] = "stix"
+plt.rcParams["figure.autolayout"] = True
+plt.rcParams["savefig.transparent"] = True
+plt.rcParams["figure.dpi"] = 600
+plt.rcParams["legend.fancybox"] = False
+plt.rcParams["legend.framealpha"] = 1
+plt.rcParams["axes.prop_cycle"] = plt.cycler('color', ["#E64B35", "#4DBBD5", "#00A087", "#3C5488", "#F39B7F", "#8491B4", "#91D1C2", "#DC0000", "#7E6148", "#B09C85"])
