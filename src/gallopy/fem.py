cf3906edd07b29d71c7dc155e9993ffc96844c12
Social Mean <2052760@tongji.edu.cn>
2023-07-11 17:21:16 +0800

    add: _cal_K_mat

--- src/gallopy/fem.py
+++ src/gallopy/fem.py
@@ -1,8 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
-from typing import Union, Callable, Sequence
+from typing import Union, Callable, Sequence, Annotated, Literal
 from numbers import Number
-from numpy.typing import ArrayLike
+from numpy.typing import ArrayLike, NDArray
 from numpy.linalg import inv, pinv
 from scipy.sparse import dia_matrix
 from scipy.sparse.linalg import lsqr, spsolve
@@ -15,9 +15,9 @@ from .matrix import kronecker_delta
     
 class FEMSolver1D(object):
     def __init__(self,
-                 alpha_func: Union[Callable, Number],
-                 beta_func: Union[Callable, Number],
-                 force_func: Union[Callable, Number],
+                 alpha_func: Union[Callable[[float], float], float],
+                 beta_func: Union[Callable[[float], float], float],
+                 force_func: Union[Callable[[float], float], float],
                  boundary_conditions: Sequence[BoundaryCondition]):
         self.alpha_func = alpha_func
         self.beta_func = beta_func
@@ -120,10 +120,10 @@ class FEMSolver1D(object):
 
 class FEMSolver2D(object):
     def __init__(self,
-                 alpha_x_func: Union[Callable, Number],
-                 alpha_y_func: Union[Callable, Number],
-                 beta_func: Union[Callable, Number],
-                 force_func: Union[Callable, Number],
+                 alpha_x_func: Union[Callable[[float, float], float], float],
+                 alpha_y_func: Union[Callable[[float, float], float], float],
+                 beta_func: Union[Callable[[float, float], float], float],
+                 force_func: Union[Callable[[float, float], float], float],
                  boundary_conditions: Sequence[BoundaryCondition]):
         self.alpha_x_func = alpha_x_func
         self.alpha_y_func = alpha_y_func
@@ -155,7 +155,6 @@ class FEMSolver2D(object):
         segments_num = np.shape(ns_mat)[0]
         # alpha, beta, f 矩阵,
         # 如果给的是函数, 则计算各个矩阵; 如果给的是常数, 则赋予常数阵
-        # TODO alpha_x 等都是向量, 索引是e
         if isinstance(self.alpha_x_func, Callable):
             alpha_x = self.alpha_x_func(x_arr, y_arr)
         else:
@@ -175,7 +174,7 @@ class FEMSolver2D(object):
             # beta = self.beta_func
         
         if isinstance(self.force_func, Callable):
-            f = self.force_func(x_arr, y_arr)
+            f = self.force_func(x_arr, y_arr)  # TODO: 应为单元的 f 值
         else:
             f = np.ones(segments_num) * self.force_func
         return alpha_x, alpha_y, beta, f
@@ -187,46 +186,57 @@ class FEMSolver2D(object):
         y_arr = triangulation.y
         
         alpha_x, alpha_y, beta, f = self._cal_param_arr(ns_mat, x_arr, y_arr)
-        a_arr_3x0, b_arr_3x0, c_arr_3x0 = self._cal_abc_arr_3x0(ns_mat, x_arr, y_arr)
-        Delta_arr = self._cal_Delta_arr(b_arr_3x0, c_arr_3x0)
+        a_arr_3xN, b_arr_3xN, c_arr_3xN = self._cal_abc_arr_3xN(ns_mat, x_arr, y_arr)
+        Delta_arr = self._cal_Delta_arr(b_arr_3xN, c_arr_3xN)
         
         N_arr = self._cal_N_arr(x_arr, y_arr)
-        K_arr_3x3x0 = self._cal_K_arr_3x3x0(alpha_x, alpha_y, beta, Delta_arr, b_arr_3x0, c_arr_3x0)
+        K_arr_3x3xN = self._cal_K_arr_3x3xN(alpha_x, alpha_y, beta, Delta_arr, b_arr_3xN, c_arr_3xN)
+        
+        K_mat = self._cal_K_mat(ns_mat, K_arr_3x3xN)
+        plt.figure()
+        tmp1 = np.max(K_mat.flatten())
+        tmp2 = np.min(K_mat.flatten())
+        tmp = max(tmp1, -tmp2)
+        plt.matshow(K_mat, cmap="seismic", vmin=-tmp, vmax=tmp)
+        # plt.show()
+        plt.savefig("./outputs/K_mat.pdf")
+        
+        
         pass
     
-    def _cal_Delta_arr(self, b_arr_3x0: ArrayLike, c_arr_3x0: ArrayLike) -> ArrayLike:
+    def _cal_Delta_arr(self, b_arr_3xN: ArrayLike, c_arr_3xN: ArrayLike) -> ArrayLike:
      
         
-        Delta_arr = (b_arr_3x0[0]*c_arr_3x0[1] - b_arr_3x0[1]*c_arr_3x0[0]) / 2
+        Delta_arr = (b_arr_3xN[0] * c_arr_3xN[1] - b_arr_3xN[1] * c_arr_3xN[0]) / 2
         return Delta_arr
     
     def _cal_N_arr(self, x_arr: ArrayLike, y_arr: ArrayLike) -> ArrayLike:
         # N_arr = kronecker_delta()
         pass
     
-    def _cal_K_arr_3x3x0(self,
+    def _cal_K_arr_3x3xN(self,
                          alpha_x: ArrayLike,
                          alpha_y: ArrayLike,
                          beta: ArrayLike,
                          Delta_arr: ArrayLike,
-                         b_arr_3x0: ArrayLike,
-                         c_arr_3x0: ArrayLike) -> ArrayLike:
+                         b_arr_3xN: ArrayLike,
+                         c_arr_3xN: ArrayLike) -> ArrayLike:
         arr_len = len(Delta_arr)
-        K_arr_3x3x0 = np.zeros((3, 3, arr_len))
+        K_arr_3x3xN = np.zeros((3, 3, arr_len))
         
         for i in range(3):
             for j in range(3):
-                tmp1 = alpha_x * b_arr_3x0[i] * b_arr_3x0[j] + alpha_y * c_arr_3x0[i] * c_arr_3x0[j]
+                tmp1 = alpha_x * b_arr_3xN[i] * b_arr_3xN[j] + alpha_y * c_arr_3xN[i] * c_arr_3xN[j]
                 tmp2 = beta*(1+kronecker_delta(i, j))
-                K_arr_3x3x0[i, j] = tmp1 / (4*Delta_arr) + Delta_arr/12 * tmp2
+                K_arr_3x3xN[i, j] = tmp1 / (4*Delta_arr) + Delta_arr/12 * tmp2
         
-        return K_arr_3x3x0
+        return K_arr_3x3xN
         
-    def _cal_abc_arr_3x0(self, ns_mat, x_arr, y_arr):
+    def _cal_abc_arr_3xN(self, ns_mat, x_arr, y_arr):
         arr_len = len(ns_mat)
-        a_arr_3x0 = np.zeros((3, arr_len))
-        b_arr_3x0 = np.zeros((3, arr_len))
-        c_arr_3x0 = np.zeros((3, arr_len))
+        a_arr_3xN = np.zeros((3, arr_len))
+        b_arr_3xN = np.zeros((3, arr_len))
+        c_arr_3xN = np.zeros((3, arr_len))
         
         ns1, ns2, ns3 = np.hsplit(ns_mat, 3)
         ns1 = ns1.flatten()
@@ -241,19 +251,34 @@ class FEMSolver2D(object):
         y2_arr = y_arr[ns2]
         y3_arr = y_arr[ns3]
         
-        a_arr_3x0[0] = x2_arr*y3_arr - y2_arr*x3_arr
-        a_arr_3x0[1] = x3_arr*y1_arr - y3_arr*x1_arr
-        a_arr_3x0[2] = x1_arr*y2_arr - y1_arr*x2_arr
+        a_arr_3xN[0] = x2_arr*y3_arr - y2_arr*x3_arr
+        a_arr_3xN[1] = x3_arr*y1_arr - y3_arr*x1_arr
+        a_arr_3xN[2] = x1_arr*y2_arr - y1_arr*x2_arr
         
-        b_arr_3x0[0] = y2_arr - y3_arr
-        b_arr_3x0[1] = y3_arr - y1_arr
-        b_arr_3x0[2] = y1_arr - y2_arr
+        b_arr_3xN[0] = y2_arr - y3_arr
+        b_arr_3xN[1] = y3_arr - y1_arr
+        b_arr_3xN[2] = y1_arr - y2_arr
         
-        c_arr_3x0[0] = x3_arr - x2_arr
-        c_arr_3x0[1] = x1_arr - x3_arr
-        c_arr_3x0[2] = x2_arr - x1_arr
+        c_arr_3xN[0] = x3_arr - x2_arr
+        c_arr_3xN[1] = x1_arr - x3_arr
+        c_arr_3xN[2] = x2_arr - x1_arr
         
-        return a_arr_3x0, b_arr_3x0, c_arr_3x0
+        return a_arr_3xN, b_arr_3xN, c_arr_3xN
         
+    def _cal_K_mat(self,
+                   ns_mat: Annotated[NDArray[float], Literal["N", 3]],
+                   K_arr_3x3xN: Annotated[NDArray[float], Literal[3, 3, "N"]]) -> np.ndarray:
+        dimension = np.max(ns_mat.flatten())+1
+        K_mat = np.zeros((dimension, dimension))
+        for e in range(np.shape(ns_mat)[0]):
+            for i in range(3):
+                row = ns_mat[e, i]
+                for j in range(3):
+                    col = ns_mat[e, j]
+                    K_mat[row, col] += K_arr_3x3xN[i, j, e]   
+        return K_mat
     
+    def _cal_b_mat_arr_3xN(self, Delta_arr, f):
+        b_mat_arr = np.zeros((3, len(Delta_arr)))
+        
     
